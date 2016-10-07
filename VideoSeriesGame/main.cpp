f1335ccbeb2879f76dc38ae9744e8bafb5f8f563
Joshua McDonald <joshuam@indeed.com>
2016-10-07 12:42:01 -0500

    Fixed instantiated game (now static) in main.

--- VideoSeriesGame/main.cpp
+++ VideoSeriesGame/main.cpp
@@ -4,11 +4,8 @@
 
 int main() {
 
-    // Constructor
-    Game *game = new Game();
-
     // Start the game
-    game->start();
+    Game::start();
 
     // When we get here, it means we've broken out of the game loop
     return 0;
