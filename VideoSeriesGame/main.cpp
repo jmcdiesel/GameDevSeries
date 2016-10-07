1d7c17423a03e4c91905dddc4580f52d9e48fcc2
Joshua McDonald <joshuam@indeed.com>
2016-10-07 12:13:24 -0500

    Initial Commit

---
+++ VideoSeriesGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  VideoSeriesGame
+//
+//  Created by Joshua McDonald on 10/7/16.
+//  Copyright (c) 2016 Joshua McDonald. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
